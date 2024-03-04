e0df6dcc91557818513d31728c000990dc40dda6
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-03-04 16:55:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//personal message: Store a person's name in a variable, and print a message to that persaon. Your message should be simple, sach as "Hello Eric, would you like to learn some Python today? "
+var personName = "eric";
+console.log("Hello ".concat(personName, ", would you like to learn some Python today?"));
+//Name Cases:Store a person's name in a variable, and then print that person's name in lowerecase, uppercase and titlecase. 
---
+++ main.ts
@@ -0,0 +1,6 @@
+//Question no. 02.
+//personal message: Store a person's name in a variable, and print a message to that persaon. Your message should be simple, sach as "Hello Eric, would you like to learn some Python today? "
+
+let personName: string ="eric";
+
+console.log(`Hello ${personName}, would you like to learn some Python today?`);
